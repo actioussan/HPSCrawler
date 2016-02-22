bb95593baac75ab41c927bf50625e462f1a3405f
actioussan <eugenia.kovalevski@gmail.com>
2016-02-22 18:10:09 +0100

    bla

--- src/main/scala/CrawlerVariable.scala
+++ src/main/scala/CrawlerVariable.scala
@@ -1,6 +1,3 @@
-/**
- * Created by test on 22.02.2016.
- */
 abstract class CrawlerVariable {}
 
 case class CrawlerString(s: String) extends CrawlerVariable {}
--- src/main/scala/Interpreter.scala
+++ src/main/scala/Interpreter.scala
@@ -1,6 +1,3 @@
-/**
- * Created by test on 22.02.2016.
- */
 trait Interpreter {
   def run(in: CrawlerVariable): CrawlerVariable
 }
